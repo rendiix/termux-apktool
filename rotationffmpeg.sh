5f9224b7bf4e488f06499542274b83155ac5cc31
FYI <vanzdobz@gmail.com>
2021-08-30 22:08:04 +0700

    Delete rotationffmpeg.sh

--- rotationffmpeg.sh
+++
@@ -1,3 +0,0 @@
-
-
-ffmpeg -i $1 -threads 8 -filter_complex "[0:v]scale=ih*16/9:-1,boxblur=luma_radius=min(h\,w)/20:luma_power=1:chroma_radius=min(cw\,ch)/20:chroma_power=1[bg];[bg][0:v]overlay=(W-w)/2:(H-h)/2,crop=h=iw*9/16" $2
